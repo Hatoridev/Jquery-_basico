ee21eb0c9e79839365c61f72b577e87cb9e7f1ea
Hatoridev <jonasdev21@outlook.com>
2024-12-27 15:04:53 -0300

    Adicionando arquivos da aula01, utilização da Biblioteca jQuery, da linguagem de progamação  JavaScript, Nível Básico

---
+++ aula01/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {  // Garante que o código será executado somente quando o DOM estiver pronto
+    $('#btn').click(function() {  // Quando o botão for clicado
+        $('#msg').text('Você clicou no botão!');  // Altera o texto do parágrafo com id 'msg'
+    });
+});
+
